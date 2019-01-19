dff8e6e82d4bb9f8da3937ef6b7ddf79c6ba4750
jamilkamal <46218967+jamilkamal@users.noreply.github.com>
2019-01-19 17:32:48 +0000

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,7 @@
 ( function (window){
 var byeSpeaker = {};
-byeSpeaker.name = 'John';
+byeSpeaker.name = "";
 var speakWord = "Good Bye";
-
 byeSpeaker.speak = function() {
 console.log(speakWord + " " + byeSpeaker.name);
  }   
